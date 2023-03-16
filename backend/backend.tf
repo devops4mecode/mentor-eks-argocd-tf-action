cd408e5f495d0cd57e52f91a7565e114a7e4beed
Najib Radzuan <najibradzuan@devops4me.com>
2023-03-16 10:52:51 +0800

    update backend

--- backend/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket               = "do4m-tf-eks-cluster"
-    key                  = "argocdinfra.json"
-    region               = "ap-southeast-1"
-    workspace_key_prefix = "env-dev"
-    dynamodb_table       = "do4m-tf-eks-cluster.lock" 
-  }
-}
